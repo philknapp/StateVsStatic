6bff2e851264322d7ca7b86e5fe83da6aff8985c
philknapp <whirlo@gmail.com>
2016-03-09 20:08:51 -0600

    Added files via upload

---
+++ StateVsStatic/WebApplication2/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="staticLabel" runat="server" Text="Static: "></asp:Label>
+        <asp:Label ID="staticCountLabel" runat="server" Text="0"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="viewStateLabel" runat="server" Text="View State: "></asp:Label>
+        <asp:Label ID="viewCountLabel" runat="server" Text="0"></asp:Label>
+    
+        <br />
+        <br />
+        <asp:Button ID="incrementButton" runat="server" OnClick="Button1_Click" Text="Increment" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ StateVsStatic/WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static int counter = 0;
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            counter = counter + 1;
+            staticCountLabel.Text = "" + counter;
+
+            if (this.ViewState["Counter"] == null)
+            {
+                this.ViewState["Counter"] = 0;
+            }
+
+            int stateCount = (int)this.ViewState["Counter"];
+            stateCount = stateCount + 1;
+            this.ViewState["Counter"] = stateCount;
+            viewCountLabel.Text = this.ViewState["Counter"].ToString();
+
+        }
+
+    }
+}
